3df3e32e7455408995cdad5ad1539f54fc7412f0
angelikavolaric <volaric.angelika@gmail.com>
2026-01-03 12:56:49 +0100

    executable program

---
+++ main.py
@@ -0,0 +1,28 @@
+from tkinter import *
+import random
+from PIL import Image, ImageDraw
+
+
+root = Tk()
+cHeight = 595
+
+cWidith = 842
+numPoints = 50 #input("Število točk: ")
+
+image = Image.new(mode="RGB", size=(cWidith, cHeight), color="white")
+draw = ImageDraw.Draw(image)
+
+def drawPoints(numPoints):
+
+    for i in range(numPoints):
+        x = random.randrange(10, cWidith-10)
+        y = random.randrange(10, cHeight-10)
+        draw.ellipse((x, y, x + 3, y + 3) ,fill="black", width=3)
+        draw.text((x-3, y-11), text=(str(i+1)), fill="black", size=10)
+    return
+
+drawPoints(int(numPoints))
+
+image.show()
+image.save('output' + str(random.randint(1, cWidith * cHeight)) +'.pdf')
+
